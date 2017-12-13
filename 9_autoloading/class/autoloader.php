68bf6cca2d48e4b8fc5ba0b6ce297485a73792b5
anouchk <analutzky@gmail.com>
2017-12-13 19:09:54 +0100

    minor bug fix

--- 9_autoloading/class/autoloader.php
+++ 9_autoloading/class/autoloader.php
@@ -1,9 +1,13 @@
 <?php
 
-class Autoloader{
+class Autoloader {
 
-	static function autoload($class_name) {
-	require 'class/' . $class_name . '.php';
+	static function register () {
+		spl_autoload_register(array('Autoloader', 'autoload' ));
+	}
+
+	static function autoload ($class_name) {
+		require 'class/' . $class_name . '.php';
 	}
 
 }
