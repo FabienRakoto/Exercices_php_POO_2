f23852db31dce5e8477e47c7fbc0a1a52f42e615
anouchk <analutzky@gmail.com>
2017-12-13 17:47:08 +0100

    heritage avec bootstrap

---
+++ 8_exemple_d_heritage_avec_bootstrap/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE-edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content"">
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" rel="stylesheet">
+	<title></title>
+</head>
+<body>
+
+
+
+<?php
+
+require 'form.php';
+require 'bootstrap_form.php';
+
+$form = new Bootstrap_form($_POST);
+
+?>
+
+<form action="#" method="post">
+	
+	<?php
+
+	echo $form->input('username') ;
+
+	echo $form->input('password') ;
+
+	echo $form->submit();
+
+	?>
+
+</form>
+
+</body>
+</html>
