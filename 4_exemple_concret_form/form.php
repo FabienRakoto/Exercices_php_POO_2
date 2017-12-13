b58166e9b1ca41b7a9e0a6379714569bd008f961
anouchk <analutzky@gmail.com>
2017-12-13 14:36:56 +0100

    renommé correctement ma fonction surround

--- 4_exemple_concret_form/form.php
+++ 4_exemple_concret_form/form.php
@@ -5,16 +5,25 @@ class Form {
 	private $data;
 	public $surround = 'p';
 
-	public function __construct($data){
+	public function __construct($data = array()){
 		$this->data = $data;
 	}
 
-	private function sourround($html){
+	public function surround($html){
 		return "<{$this->surround}>{$html}</{$this->surround}>";
 	}
 
+	// ternaire
+	private function getValue($index) {
+		return isset($this->data[$index]) ? $this->data[$index] : null;
+	}
+
 	public function input($name){
-		return $this->surround('<input type="text" name="' . $name . '">');
+		return $this->surround(
+			'<input type="text" name="' . $name . '" value="
+				' . $this->getValue($name) . '
+			">'
+		);
 	}
 
 	public function submit(){
