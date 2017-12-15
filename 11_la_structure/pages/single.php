2d4a67bec3f6d4c86d95ba4adfc27c1a921b0816
anouchk <analutzky@gmail.com>
2017-12-15 18:15:45 +0100

    structure son blog en POO

---
+++ 11_la_structure/pages/single.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Starter Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+      <a class="navbar-brand" href="#">Single</a>
+    </nav>
+
+    <main role="main" class="container">
+
+      <div class="starter-template" style="padding-top: 100px;">
+        <h1>Notre single</h1>
+        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+      </div>
+
+    </main><!-- /.container -->
+
+  </body>
+</html>
---
+++ 11_la_structure/public/index1.php
@@ -0,0 +1,17 @@
+<?php
+
+require '../app/autoloader.php';
+
+APP\Autoloader::register();
+
+if(isset($_GET['p'])) {
+	$p = $_GET['p'];
+} else {
+	$p = 'home';
+}
+
+if ($p === 'home') {
+	require '../pages/home.php';
+} elseif ($p === 'single') {
+	require '../pages/single.php';
+}
